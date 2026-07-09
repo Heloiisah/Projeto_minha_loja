7f71cb6196d88d9169d6d7136c1a11c14311d29b
heloisa <heloisa.f.santos10@aluno.senai.br>
2026-07-09 16:03:02 -0300

    alteração

---
+++ js/lista_produtos.js
@@ -0,0 +1,182 @@
+const produtos = [
+    {
+        idProduto: 1,
+        descricaoProduto: "Agenda Personalizada",
+        valorUnitario: 49.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/agenda.png",
+        idSecao: 1,
+        secao: "Papelaria"
+    },
+    {
+        idProduto: 2,
+        descricaoProduto: "Mini Bíblia",
+        valorUnitario: 24.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/biblia.png",
+        idSecao: 2,
+        secao: "Religiosos"
+    },
+    {
+        idProduto: 3,
+        descricaoProduto: "Botton Chaveiro",
+        valorUnitario: 12.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/botton chaveiro.png",
+        idSecao: 3,
+        secao: "Brindes"
+    },
+    {
+        idProduto: 4,
+        descricaoProduto: "Botton Ímã",
+        valorUnitario: 8.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/botton ima.png",
+        idSecao: 3,
+        secao: "Brindes"
+    },
+    {
+        idProduto: 5,
+        descricaoProduto: "Caneca Alumínio",
+        valorUnitario: 34.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/caneca aluminio.png",
+        idSecao: 4,
+        secao: "Canecas"
+    },
+    {
+        idProduto: 6,
+        descricaoProduto: "Caneca com Colher",
+        valorUnitario: 42.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/caneca colher.png",
+        idSecao: 4,
+        secao: "Canecas"
+    },
+    {
+        idProduto: 7,
+        descricaoProduto: "Caneca Colorida",
+        valorUnitario: 39.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/caneca colorida.png",
+        idSecao: 4,
+        secao: "Canecas"
+    },
+    {
+        idProduto: 8,
+        descricaoProduto: "Caneca Shopp",
+        valorUnitario: 44.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/caneca shopp.png",
+        idSecao: 4,
+        secao: "Canecas"
+    },
+    {
+        idProduto: 9,
+        descricaoProduto: "Caneca Personalizada",
+        valorUnitario: 35.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/caneca.png",
+        idSecao: 4,
+        secao: "Canecas"
+    },
+    {
+        idProduto: 10,
+        descricaoProduto: "Cartão SUS Personalizado",
+        valorUnitario: 9.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/cartao sus.png",
+        idSecao: 5,
+        secao: "Documentos"
+    },
+    {
+        idProduto: 11,
+        descricaoProduto: "Chaveiro Acrílico",
+        valorUnitario: 14.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/chaveiro acrilico.png",
+        idSecao: 3,
+        secao: "Brindes"
+    },
+    {
+        idProduto: 12,
+        descricaoProduto: "Garrafa Personalizada 1",
+        valorUnitario: 69.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/garrafa.png",
+        idSecao: 6,
+        secao: "Garrafas"
+    },
+    {
+        idProduto: 13,
+        descricaoProduto: "Garrafa Personalizada 2",
+        valorUnitario: 74.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/garrafa2.png",
+        idSecao: 6,
+        secao: "Garrafas"
+    },
+    {
+        idProduto: 14,
+        descricaoProduto: "Kit Presente",
+        valorUnitario: 89.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/kit.png",
+        idSecao: 7,
+        secao: "Kits"
+    },
+    {
+        idProduto: 15,
+        descricaoProduto: "Kit Personalizado 1",
+        valorUnitario: 79.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/kit1.png",
+        idSecao: 7,
+        secao: "Kits"
+    },
+    {
+        idProduto: 16,
+        descricaoProduto: "Kit Personalizado 2",
+        valorUnitario: 84.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/kit2.png",
+        idSecao: 7,
+        secao: "Kits"
+    },
+    {
+        idProduto: 17,
+        descricaoProduto: "Kit Personalizado 3",
+        valorUnitario: 94.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/kit3.png",
+        idSecao: 7,
+        secao: "Kits"
+    },
+    {
+        idProduto: 18,
+        descricaoProduto: "Mini Álbum",
+        valorUnitario: 29.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/mini album.png",
+        idSecao: 8,
+        secao: "Álbuns"
+    },
+    {
+        idProduto: 19,
+        descricaoProduto: "Torre Personalizada",
+        valorUnitario: 54.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/torre.png",
+        idSecao: 9,
+        secao: "Decoração"
+    },
+    {
+        idProduto: 20,
+        descricaoProduto: "Torre Personalizada Premium",
+        valorUnitario: 59.90,
+        unidade: "UN",
+        caminhoImagem: "imagens/torre1.png",
+        idSecao: 9,
+        secao: "Decoração"
+    }
+];
