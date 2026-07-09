cfca9e408c725a0db7756e3865a188c0bcb3069d
heloisa.f.santos@aluno.senai.br <heloisa.f.santos10@aluno.senai.br>
2026-07-09 19:01:16 -0300

    alteração

--- js/produtos.js
+++ js/produtos.js
@@ -1,31 +1,184 @@
-//IMPORTANDO OS PRODUTOS DO ARQUIVO lista_produtos.js
-import { produtos } from "./lista_produtos";
+const produtos = [
+    {
+        id_produto: 1,
+        descricao_produto: "Agenda Personalizada",
+        valor_unitario: 49.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/agenda.png",
+        id_secao: 1,
+        secao: "Papelaria"
+    },
+    {
+        id_produto: 2,
+        descricao_produto: "Mini Bíblia",
+        valor_unitario: 24.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/biblia.png",
+        id_secao: 2,
+        secao: "Religiosos"
+    },
+    {
+        id_produto: 3,
+        descricao_produto: "Botton Chaveiro",
+        valor_unitario: 12.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/botton chaveiro.png",
+        id_secao: 3,
+        secao: "Brindes"
+    },
+    {
+        id_produto: 4,
+        descricao_produto: "Botton Ímã",
+        valor_unitario: 8.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/botton ima.png",
+        id_secao: 3,
+        secao: "Brindes"
+    },
+    {
+        id_produto: 5,
+        descricao_produto: "Caneca Alumínio",
+        valor_unitario: 34.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/caneca aluminio.png",
+        id_secao: 4,
+        secao: "Canecas"
+    },
+    {
+        id_produto: 6,
+        descricao_produto: "Caneca com Colher",
+        valor_unitario: 42.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/caneca colherr.png",
+        id_secao: 4,
+        secao: "Canecas"
+    },
+    {
+        id_produto: 7,
+        descricao_produto: "Caneca Colorida",
+        valor_unitario: 39.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/caneca colorida.png",
+        id_secao: 4,
+        secao: "Canecas"
+    },
+    {
+        id_produto: 8,
+        descricao_produto: "Caneca Shopp",
+        valor_unitario: 44.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/caneca shopp.png",
+        id_secao: 4,
+        secao: "Canecas"
+    },
+    {
+        id_produto: 9,
+        descricao_produto: "Caneca Personalizada",
+        valor_unitario: 35.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/caneca.png",
+        id_secao: 4,
+        secao: "Canecas"
+    },
+    {
+        id_produto: 10,
+        descricao_produto: "Cartão SUS Personalizado",
+        valor_unitario: 9.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/cartao sus.png",
+        id_secao: 5,
+        secao: "Documentos"
+    },
+    {
+        id_produto: 11,
+        descricao_produto: "Chaveiro Acrílico",
+        valor_unitario: 14.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/chaveiro acrilico.png",
+        id_secao: 3,
+        secao: "Brindes"
+    },
+    {
+        id_produto: 12,
+        descricao_produto: "Garrafa Personalizada 1",
+        valor_unitario: 69.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/garrafa.png",
+        id_secao: 6,
+        secao: "Garrafas"
+    },
+    {
+        id_produto: 13,
+        descricao_produto: "Garrafa Personalizada 2",
+        valor_unitario: 74.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/garrafa2.png",
+        id_secao: 6,
+        secao: "Garrafas"
+    },
+    {
+        id_produto: 14,
+        descricao_produto: "Kit Presente",
+        valor_unitario: 89.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/kit.png",
+        id_secao: 7,
+        secao: "Kits"
+    },
+    {
+        id_produto: 15,
+        descricao_produto: "Kit Personalizado 1",
+        valor_unitario: 79.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/kit1.png",
+        id_secao: 7,
+        secao: "Kits"
+    },
+    {
+        id_produto: 16,
+        descricao_produto: "Kit Personalizado 2",
+        valor_unitario: 84.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/kit2.png",
+        id_secao: 7,
+        secao: "Kits"
+    },
+    {
+        id_produto: 17,
+        descricao_produto: "Kit Personalizado 3",
+        valor_unitario: 94.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/kit3.png",
+        id_secao: 7,
+        secao: "Kits"
+    },
+    {
+        id_produto: 18,
+        descricao_produto: "Mini Álbum",
+        valor_unitario: 29.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/mini album.png",
+        id_secao: 8,
+        secao: "Álbuns"
+    },
+    {
+        id_produto: 19,
+        descricao_produto: "Torre Personalizada",
+        valor_unitario: 54.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/torre.png",
+        id_secao: 9,
+        secao: "Decoração"
+    },
+    {
+        id_produto: 20,
+        descricao_produto: "Torre Personalizada Premium",
+        valor_unitario: 59.90,
+        unidade: "UN",
+        caminho_imagem: "../imagens/torre1.png",
+        id_secao: 9,
+        secao: "Decoração"
+    }
+];
 
-// PEGANDO ELEMENTOS DO DOM
-const sectionCards = document.querySelector('#cards')
-
-// CARREGANDO OS CARDS
-const listarProdutos = () => {
-
-    sectionCards.innerHTML = ''
-
-    produtos.forEach((elem, i) => 
-
-
-
-        const divCards = document.createElement('div')
-        divCards.setAttribute('class', 'card')
-
-        const imgCard = document.createElement('img')
-        imgCard.setAttribute('src', elem.caminho_imagem)
-        imgCard.setAttribute('alt', elem.descricao_produto)
-
-        const pCard = document.createElement('p')
-        pCard.innerHTML = elem.descricao_produto
-
-        const h2Card = document.createElement('h2')
-        h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario)
-            .toFixed(2)
-            .replace('.', ',')}`
-    )}
-      
+export { produtos };
