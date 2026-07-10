b024fcfcaa3273830f835932e5f730e152b032c3
heloisa <heloisa.f.santos10@aluno.senai.br>
2026-07-10 15:41:00 -0300

    alterção

--- js/produtos.js
+++ js/produtos.js
@@ -6,50 +6,8 @@ const sectionCards = document.querySelector("#cards");
 
 //CARREGANDO OS CARDS
 const listarProdutos = () => {
-
-    //LIMPANDO A SECTION cards
-    sectionCards.innerHTML = ''
-
-    //PECORRENDO O ARRAY DE PRODUTOS
-    produtos.forEach((elem, i) => {
-
-        //CRIANDO O ELEMENTO DIV E DEFININDO O ATRIBUTO CARD
-        const divCard = document.createElement('div')
-        divCard.setAttribute('class', 'card')
-
-        //CRIANDO O ELEMENTO img E DEFININDO OS ATRIBUTOS SRC E ALT OS VALORES DO CAMINHO DAS IMAGENS E A DESCRIÇÃO DOS PRODUTOS
-        const imgCard = document.createElement('img')
-        imgCard.setAttribute('src', elem.caminho_imagem)
-        imgCard.setAttribute('alt', elem.descricao_produto)
-
-        //CRIANDO O ELEMENTO p E ATRIBUINDO A DESCRIÇÃO DOS PRODUTOS
-        const pCard = document.createElement('p')
-        pCard.innerHTML = elem.descricao_produto
-
-        //CRIANDO O ELEMENTO h2 E ATRIBUINDO O VALOR UNITÁRIO DEIXANDO EM DUAS CASAS DECIMAIS E SUBSTITUINDO PONTO POR VÍRGULA
-        const h2Card = document.createElement('h2')
-        h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`
-
-        //CRIANDO O ELEMENTO button E DEFININDO OS ATRIBUTOS CLASS E A DESCRIÇÃO ADICIONAR
-        const btnCard = document.createElement('button')
-        btnCard.setAttribute('class', 'btn-add')
-        btnCard.innerHTML = 'Adicionar'
-
-        //ADICIONANDO OS ELEMENTOS FILHOS AOS divCard
-        divCard.appendChild(imgCard)
-        divCard.appendChild(pCard)
-        divCard.appendChild(h2Card)
-        divCard.appendChild(btnCard)
-
-        //ADICIONANDO O divCard A SECTION CARDS
-        sectionCards.appendChild(divCard)
-
-    })
-
+    montaCards(produtos)
 }
-
-//CHAMANDO A FUNÇÃO listarProdutos
-listarProdutos()
 
 //MONTANDO OS MENUS SEÇÕES
 const menuSecoes = () => {
@@ -89,9 +47,8 @@ const carregaSecoes = () => {
         aMenu.setAttribute('class', 'lnk-secao')
         aMenu.innerHTML = elem.secao
 
-        aMenu.addEventListener('click', ()=>{
-            filtroProduto(elem.id_secao)
-
+        aMenu.addEventListener('click', () => {
+            montaCards(filtroProduto(elem.id_secao))
         })
 
         //ADICIONANDO O ELEMENTO FILHO a NO li
@@ -104,10 +61,58 @@ const carregaSecoes = () => {
 
 }
 
-carregaSecoes()
-
 //FUNÇÃO FILTRO PRODUTO
-const filtroProduto =(idSecao)=>{
-    //FILTRANDO OS PRODUTOS A PARTIR DO REPETIÇÃO filter
-    return produtos.filter(elem => elem.id_secao===idSecao)
+const filtroProduto = (idSecao) => {
+
+    //FILTRANDO OS PRODUTOS A PARTIR DA REPETIÇÃO filter
+    return produtos.filter(elem => elem.id_secao === idSecao)
+
 }
+
+//FUNÇÃO MONTA CARD
+const montaCards = (objProdutos) => {
+
+    //LIMPANDO A SECTION cards
+    sectionCards.innerHTML = ''
+
+    //PECORRENDO O ARRAY DE objProdutos
+    objProdutos.forEach((elem, i) => {
+
+        //CRIANDO O ELEMENTO DIV E DEFININDO O ATRIBUTO CARD
+        const divCard = document.createElement('div')
+        divCard.setAttribute('class', 'card')
+
+        //CRIANDO O ELEMENTO img E DEFININDO OS ATRIBUTOS SRC E ALT
+        const imgCard = document.createElement('img')
+        imgCard.setAttribute('src', elem.caminho_imagem)
+        imgCard.setAttribute('alt', elem.descricao_produto)
+
+        //CRIANDO O ELEMENTO p
+        const pCard = document.createElement('p')
+        pCard.innerHTML = elem.descricao_produto
+
+        //CRIANDO O ELEMENTO h2
+        const h2Card = document.createElement('h2')
+        h2Card.innerHTML = `R$ ${parseFloat(elem.valor_unitario).toFixed(2).replace('.', ',')}`
+
+        //CRIANDO O ELEMENTO button
+        const btnCard = document.createElement('button')
+        btnCard.setAttribute('class', 'btn-add')
+        btnCard.innerHTML = 'Adicionar'
+
+        //ADICIONANDO OS ELEMENTOS FILHOS AO divCard
+        divCard.appendChild(imgCard)
+        divCard.appendChild(pCard)
+        divCard.appendChild(h2Card)
+        divCard.appendChild(btnCard)
+
+        //ADICIONANDO O divCard À SECTION CARDS
+        sectionCards.appendChild(divCard)
+
+    })
+
+}
+
+//CHAMANDO AS FUNÇÕES
+listarProdutos()
+carregaSecoes()
