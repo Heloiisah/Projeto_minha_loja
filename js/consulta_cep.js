71b182a43fc242f53d94f9963ca3d5318f879a6b
heloisa <heloisa.f.santos10@aluno.senai.br>
2026-07-17 16:30:46 -0300

    alteração diversas

---
+++ js/consulta_cep.js
@@ -0,0 +1,60 @@
+//PEGANDO ELEMENETO DO DOM
+const inputCep = document.querySelector('#cep')
+
+//CAPTURANDO O EVENTO change
+inputCep.addEventListener('change', (evt) => {
+
+    //PEGANDO OS NÚMEROS DOS INPUTCEP
+    const numCep = evt.target.value.replace(/\D/g, "")
+
+    //VERIFICA SE POSSUI 8 DIGITOS
+    if (numCep.length !== 8) {
+        alert("CEP INVALIDO")
+
+        return
+    }
+
+    //CHAMA A FUNÇÃO consultaCEP
+    consultaCEP(numCep)
+
+})
+
+
+//FUNÇÃO CONSULTA CEP VIACEP
+const consultaCEP = async (cep) => {
+
+    //TENTA CONECTAR A API
+    try {
+    
+        //FAZ A COMUNICAÇÃO COM A API DO VIA CEP
+        //AWAIT - GUARDA ATÉ OBTER UM PROMISSE
+        const resposta = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+
+        //SE O STATUS DA RESPOSTA NÃO FOR OK. DISPARA UMA EXCESSÃO
+        if (!resposta.ok) {
+            throw new Error("ERRO NA REQUISIÇÃO")
+        }
+
+        //OBTEM OS DADOS DA API
+        const dadosEndereco = await resposta.json()
+
+
+        //VERIFICANDO SE OS DADOS SÃO VÁLIDOS
+        if (dadosEndereco.erro) {
+            alert("CEP NÃO LOCALIZADO")
+
+            return
+        }
+
+        //CHAMA A FUNÇÃO carregaInput
+        carregaInput(dadosEndereco)
+
+    //CASO HAJA QUALQUER ERRO É DISPARADA UMA EXCEÇÃO
+
+    } catch (erro) {
+        console.log("ERRO", erro.message)
+    }
+
+}
+
+//
